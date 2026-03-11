d7df47329fe01d58d5197814d0563c6dabc67249
Abir Hasan <abirhasanrawvy@gmail.com>
2026-03-12 02:04:47 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const API_URL = "https://jsonplaceholder.typicode.com/posts";
 let tasks = [];
 
+document.addEventListener("DOMContentLoaded", function(){
+    fetchTasks();
+});
+
+function showLoading(show){
+    const loading = document.getElementById("loading");
+    loading.classList.toggle("hidden", !show);
+}
+
 function fetchTasks(){
+    showLoading(true);
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", API_URL+"?_limit=4", true);
+    xhr.open("GET", API_URL, true);
     xhr.onreadystatechange = function () {
 
         if(xhr.readyState === 4) {
+            showLoading(false);
             if(xhr.status >= 200 && xhr.status < 300){
-                try {
-                    const data = JSON.parse(xhr.responseText);
+                
+                const data = JSON.parse(xhr.responseText);
                     tasks = data.map(task=>({
                         id: task.id,
                         title: task.title,
                         completed: task.completed,
                     }));
-                } catch (error) {
-                    
-                }
+                    console.log(tasks);
+            }else{
+                console.log("Error");
             }
         }
     };
+    xhr.send();
 }
+
+
